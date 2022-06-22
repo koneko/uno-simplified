f9665e796d0311b129b4674b734aba7dfa29ba27
koneko <67551503+koneko@users.noreply.github.com>
2022-06-22 21:48:03 +0200

    added chatting to lobby and game is pretty much a blueprint for any 4 player game lobby system, feel free to screenshot 🐵🐒🙊🙉🙈

--- assets/lobby.js
+++ assets/lobby.js
@@ -1,9 +1,164 @@
 const socket = io()
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('id');
+let you = { host: null, order: null, cards: [], key: null, id: null }
+let alternate = "white"
+let players = []
 
-socket.emit('join', { id, name: localStorage.us_username })
+if (!id || id === null || localStorage.us_username == null) window.location.href = "/"
+else socket.emit('join', { id, name: localStorage.us_username })
 
 socket.on("joinResponse", data => {
+    let { host, success, names, order, key } = data;
+    if (!success) return window.location.href = "/"
+    you.host = host;
+    you.order = order;
+    you.key = key;
+    you.id = id;
+    let pre = document.querySelector(".pre-container")
+    let plist = document.querySelector(".playerlist")
+    let currentord = 1
+    let first = null
+    players.push({ name: localStorage.us_username, cards: [], cardCount: 0, order: order, you: true })
+    if (names) {
+        names.forEach(name => {
+            let div = document.createElement("div")
+            div.classList.add("player")
+            div.innerHTML = `
+                <div class="left-player">${name}</div>
+                <div class="right-player" id="${name}-player"></div>
+            `
+            plist.appendChild(div)
+            if (!host) {
+                let div = document.getElementById(`${name}-player`)
+                div.innerHTML = `Order: ${currentord}`
+                div.style.textAlign = "center"
+                currentord++
+            }
+        })
+        first = false
 
+    }
+    if (host) {
+        if (first == null) first = true
+        let button = document.createElement("button")
+        button.classList.add("start-button")
+        button.innerHTML = "Start Game"
+        button.addEventListener("click", () => {
+            startGame()
+        })
+        pre.appendChild(button)
+        let div = document.createElement("div")
+        div.classList.add("player")
+        div.innerHTML = `
+            <div class="left-player">${localStorage.us_username}</div>
+            <div class="right-player" id="${localStorage.us_username}-player"></div>
+        `
+        plist.appendChild(div)
+    }
+})
+
+socket.on("playerJoin", data => {
+    let { name, order } = data;
+    let plist = document.querySelector(".playerlist")
+    let div = document.createElement("div")
+    div.classList.add("player")
+    div.innerHTML = `
+        <div class="left-player">${name}</div>
+        <div class="right-player" id="${name}-player"></div>
+    `
+    plist.appendChild(div)
+    let playerdiv = document.getElementById(name + "-player")
+    let button = document.createElement("button")
+    button.classList.add("kick-button")
+    button.innerHTML = "Kick"
+    button.addEventListener("click", () => {
+        if (you.host) {
+            socket.emit("kick", { name, key: you.key, id: you.id })
+        }
+    })
+    if (you.host) playerdiv.appendChild(button)
+    players.push(formatPlayer(name, order))
+})
+
+socket.on("playerKick", data => {
+    console.log("Recieved kick")
+    let name = data.name
+    if (name == localStorage.us_username) {
+        alert("You got kicked, skill issue.")
+        return window.location.href = "/"
+    }
+    let playerdiv = document.getElementById(name + "-player")
+    let parent = playerdiv.parentElement
+    parent.remove()
+    removePlayer(name)
+})
+
+socket.on("leaveRoom", data => {
+    let name = data.name;
+    let playerdiv = document.getElementById(name + "-player")
+    let parent = playerdiv.parentElement
+    parent.remove()
+    removePlayer(name)
+})
+
+socket.on("chatMessage", data => {
+    let { message, name } = data;
+    let chat = document.querySelector(".chat")
+    let div = document.createElement("div")
+    div.classList.add("chat-message")
+    let user = document.createElement("span")
+    user.classList.add("chat-user")
+    user.textContent = name + ": "
+    if (name == localStorage.us_username) user.style.color = "dodgerblue"
+    let value = document.createElement("span")
+    value.classList.add("chat-value")
+    value.textContent = message
+    div.style.backgroundColor = alternate
+    div.appendChild(user)
+    div.appendChild(value)
+    chat.appendChild(div)
+    chat.scrollTop = chat.scrollHeight
+    if (alternate == "white") alternate = "lightgray"
+    else alternate = "white"
+})
+
+socket.on("roomDelete", () => {
+    alert("Please refresh page, host left the room.")
+    window.location.reload()
+})
+
+socket.on("turnChange", data => {
+    let { name, order } = data; // handle
+})
+
+function startGame () {
+    socket.emit("startGame", { key: you.key, id: you.id })
+}
+
+function sendMessage () {
+    let input = document.getElementById("message-input")
+    let message = input.value
+    socket.emit("message", { message, key: you.key, id: you.id })
+    input.value = ""
+}
+
+function formatPlayer (name, order) {
+    return {
+        name: name,
+        cards: [],
+        cardCount: 0,
+        order: order,
+        you: false
+    }
+}
+
+function removePlayer (name) {
+    players.splice(players.findIndex(player => player.name == name), 1)
+}
+
+document.getElementById("message-input").addEventListener("keydown", e => {
+    if (e.keyCode == 13) {
+        sendMessage()
+    }
 })
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/assets/main.html")
 })
 
-app.get("/lobby", (req, res) => {
+app.get("/game", (req, res) => {
     res.sendFile(__dirname + "/assets/lobby.html")
 })
 
@@ -22,29 +22,119 @@ const io = new Server(server)
 
 io.on('connection', socket => {
     socket.on('join', data => {
+        console.log(`${data.name}(${socket.id}) is attempting to join room ${data.id}`)
         //data = { id, name }
         const { id, name } = data;
         let room = rooms.find(room => room.id === id)
         if (!room) {
-
+            let created = createRoom(id, name, socket.id)
+            rooms.push(created)
+            socket.emit('joinResponse', { host: true, success: true, names: null, order: 1, key: created.players[0].key })
+            console.log(`${name}(${socket.id}) created room ${id}`)
+            socket.join(id)
+        } else {
+            //check if playername exists already
+            let player = room.players.find(player => player.name === name)
+            if (player) return socket.emit('joinResponse', { host: false, success: false, names: null, order: null, key: null })
+            if (room.players.length < 4) {
+                console.log(`${name}(${socket.id}) successfuly joined room ${id}`)
+                let key = generateKey(name, socket.id)
+                room.players.push({ name: name, cards: [], host: false, order: room.players.length + 1, socket: socket.id, key: key })
+                let pnames = room.players.map(p => p.name)
+                socket.emit('joinResponse', { host: false, success: true, names: pnames, order: room.players.length, key: key })
+                io.to(id).emit('playerJoin', { name: name, order: room.players.length })
+                socket.join(id)
+            } else {
+                socket.emit('joinResponse', { host: false, success: false, names: null, order: null, key: null })
+            }
+        }
+    })
+    socket.on("kick", data => {
+        console.log(`${socket.id} is attempting to kick ${data.name} from room ${data.id}`)
+        const { id, name, key } = data;
+        console.log(id, name, key)
+        let room = rooms.find(room => room.id === id)
+        if (!room) return
+        let playerToKick = room.players.find(player => player.name === name)
+        if (!playerToKick) return
+        let host = room.players.find(player => player.host === true)
+        if (!host) return
+        if (host.key != key) return
+        console.log(`${host.name}(${socket.id}) kicked ${playerToKick.name}(${playerToKick.socket}) from room ${id}`)
+        io.to(id).emit('playerKick', { name: playerToKick.name })
+        let socketToKick = io.sockets.sockets.get(playerToKick.socket)
+        socketToKick.leave(id)
+        room.players = room.players.filter(player => player.name !== name)
+    })
+    socket.on("message", data => {
+        const { message, id, key } = data
+        let room = rooms.find(room => room.id === id)
+        if (!room) return
+        let player = room.players.find(player => player.key === key)
+        if (!player) return
+        if (message.replace(/\s/g, "").length == 0) return
+        io.to(id).emit('chatMessage', { name: player.name, message: message })
+    })
+    socket.on("disconnect", () => {
+        console.log(`${socket.id} disconnected`)
+        let room = rooms.find(room => room.players.find(p => p.socket === socket.id))
+        if (room) {
+            console.log(`${socket.id} left room ${room.id}`)
+            let p = room.players.find(p => p.socket === socket.id)
+            if (p.host == true) {
+                //delete room and tell players
+                rooms = rooms.filter(room => room.id !== room.id)
+                io.to(room.id).emit('roomDelete')
+            } else {
+                room.players.splice(room.players.indexOf(p), 1)
+                io.to(room.id).emit('leaveRoom', { name: p.name })
+                if (room.players.length == 0) {
+                    console.log(`Room ${room.id} is empty, removing it.`)
+                    rooms.splice(rooms.indexOf(room), 1)
+                }
+            }
         }
     })
 })
 
-function createRoom (id, name) {
+function createRoom (id, name, socketid) {
     let cc = randomCard()
-    while (nc == "black-4" || nc == "black-0" || nc.contains("reverse") || nc.contains("skip") || nc.contains("draw")) {
-        nc = randomCard()
+    while (cc == "black-4" || cc == "black-0" || cc.includes("reverse") || cc.includes("skip") || cc.includes("draw")) {
+        cc = randomCard()
     }
     let room = {
         id: id,
-        players: [{ name: name, cards: [], host: true, order: 0 }],
+        players: [{ name: name, cards: [], host: true, order: 1, socket: socketid, key: generateKey(name, id) }],
         nextCard: randomCard(),
         currentCard: cc,
         nextPlayer: 2,
-        currentPlayer: 0,
+        currentPlayer: 1,
+        state: "waiting",
+        winner: null
     }
     return room
+}
+
+function generateKey (name, id) {
+    let timestamp = new Date().getTime();
+    //split name after every character
+    let nameSplit = name.split("")
+    //split id after every 3 characters
+    let idSplit = id.split("")
+    let key = ""
+    for (let i = 0; i < nameSplit.length; i++) {
+        key += nameSplit[i].replace(/k/g, "53").replace(/n/g, "12")
+        if (random(0, 9) == 3) key += random(5, 456).toString()
+        if (i % 2 == 0) {
+            key += "-"
+        }
+    }
+    for (let i = 0; i < idSplit.length; i++) {
+        key += idSplit[i]
+        if (random(0, 1) == 1) key += random(35, 126).toString()
+    }
+    key += "-" + timestamp.toString().replace(/6/g, "XVI").replace(/9/g, "LX")
+    return key.replace(/3/g, "1")
 }
 
 function randomCard () {
